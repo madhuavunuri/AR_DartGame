e84c98c0472e2f10b8dd4a07de703e2b225f8fca
Madhu Avunuri <madhu.avunuri524@gmail.com>
2022-01-16 22:15:05 +0100

    Initial Commit
    Base Project

---
+++ Assets/Scripts/PlaceObjectOnPlanes.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+public class PlaceObjectOnPlanes : MonoBehaviour
+{
+    public GameObject placementIndicator;
+    
+    Pose placementPose;
+    Transform placementTransform;
+    bool placementPoseIsValid = false;
+    TrackableId placePlaneId = TrackableId.invalidId;
+
+    ARRaycastManager m_RaycastManager;
+    static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
+
+    void Awake()
+    {
+        m_RaycastManager = GetComponent<ARRaycastManager>();        
+    }
+    void Update()
+    {
+        UpdatePlacementIndicator();
+        UpdatePlacementPosition();
+    }
+
+    void UpdatePlacementPosition()
+    {
+        if (m_RaycastManager.Raycast(Camera.main.ViewportToScreenPoint(new Vector3(0.5f,0.5f)),s_Hits,TrackableType.PlaneWithinPolygon))
+        {
+            if (placementPoseIsValid)
+            {
+                placementPose = s_Hits[0].pose;
+                placePlaneId = s_Hits[0].trackableId;
+
+                var planManager = GetComponent<ARPlaneManager>();
+                ARPlane arPlane = planManager.GetPlane(placePlaneId);
+                placementTransform = arPlane.transform;
+            }
+        }
+    }
+
+    void UpdatePlacementIndicator()
+    {
+        if(placementPoseIsValid)
+        {
+            placementIndicator.SetActive(true);
+            placementIndicator.transform.SetPositionAndRotation(placementPose.position, placementTransform.rotation);
+        }
+        else
+        {
+            placementIndicator.SetActive(false);
+        }
+    }
+}
